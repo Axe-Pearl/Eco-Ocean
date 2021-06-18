421d64b1787d3b744b57acfc14d4f8952d266b29
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-19 04:07:41 +0530

    Sample QRcode added

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import QRCode from "qrcode.react";
 import "./home.css";
 function Home() {
     return (
@@ -17,6 +18,21 @@ function Home() {
                     <button className="donate_button" type="button" ></button>
                    </div>
               </div>
+            </div>
+            <div className="learn-more">
+                <div className="qrcode">
+                    <img  src="../Images/cellphone.jpg"></img>
+                    <div className="code">
+                       <QRCode
+                        id="1234"
+                        value="Sample QRCode"
+                        size={160}
+                        level={"H"}
+                        includeMargin={true}
+                        />
+
+                    </div>
+                </div>
             </div>
         </div>
     )
