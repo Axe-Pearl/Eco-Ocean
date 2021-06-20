80c7ed2cb8b0e7bffd549a5bd029386b68a4ffcc
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-20 18:55:48 +0530

    description samples added in read more page

--- src/brief.js
+++ src/brief.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStateValue } from './StateProvider';
+import "./brief.css";
 
 function Brief() {
     const [{basket,reader},dispatch] = useStateValue();
@@ -8,11 +9,13 @@ function Brief() {
     return (
         <div className ="brief-container">
             <div className="pet_img">
-                <img src={reader[0].image}></img>
-                <h1>{reader[0].breedname}</h1>
+                <img className="image" src={reader[0].image}></img>
+                <h1 className="breed">{reader[0].petbreed}</h1>
+                <h5 className="tagline">Hey Humans! Before you adopt me,let's learn about me </h5>
             </div>
             <div className="description-box">
-                <p>{reader[0].description}</p>
+                <h3 className="introduction">Who I'm Exactly ?</h3>
+                <p className="about_pet">{reader[0].description}</p>
             </div>
         </div>
     )
--- src/pets.js
+++ src/pets.js
@@ -17,16 +17,50 @@ const pets=[
         image:"https://www.indiamike.com/india/attachments/8966d1230874802-should-we-adopt-a-stray-indian-pup-P1010578.jpg",
         petbreed:"Sample2",
         price:600,
-        decription:""
+        description:`History
+        Pembroke and Cardigan Welsh Corgis were originally listed as one breed by The Kennel Club in Britain in 1925, but were declared separate breeds in 1935.[3] The Corgi Club was founded in December 1925 in Carmarthen in South Wales.[4] It is reported that the local members favored the Pembroke breed, so a club for Cardigan enthusiasts was founded a year later (1926).[4] Both groups have worked hard to ensure the appearance and type of breed are standardized through careful selective breeding.[4] Pembrokes and Cardigans were officially recognized by the Kennel Club in 1928 and were lumped together under the heading Welsh Corgis.[4] In 1934, the two breeds were recognized individually and shown separately.
+        
+        One theory is that both breeds of the corgis descended from a line of northern spitz-type dogs.[5] Another is that they descended from the Teckel family of dogs, which also produced Dachshunds.[6]
+        
+        The word "corgi" is derived from the Welsh: cor gi, which means "dwarf dog".[7] The breed was formerly called "yard-long dog" (Welsh: ci-llathed).[8] Today's name comes from their area of origin: Cardigan (Welsh 'Ceredigion').
+        
+        Description
+        The Cardigan is a long, low dog with upright ears and a fox brush tail.[9]
+        
+        Originally used only as a farm guardian, they eventually took on the traits of a cattle drover, herder, and many more.[10] They are still highly valued for their herding, working, and guarding skills, as well as their companionship. The old American Kennel Club standard called it an "Alsatian on short legs".[citation needed] The Cardigan's tail is long (unlike the Pembroke Welsh Corgi, whose tail may be long,[11] naturally bobbed or docked[12]).
+        
+        
+        Blue merle Cardigan
+        Cardigans, which are double coated, come in a variety of colors including any shade of red, sable, or brindle, as well as black, with or without tan, brindle or blue merle, with or without tan or brindle points. Other unofficial colors can occur, such as red merle, but these colors are not considered acceptable per the Cardigan standard. They usually have white on the neck, chest, legs, muzzle, underneath, tip of the tail and as a blaze on the head, known as the "Irish pattern."[citation needed] Other markings include ticking on the legs and muzzle, smutty muzzles and monk's hoods, especially on sables (a pattern of darker tipped hairs over a basic red coat color.)[citation needed]. An average Cardigan is around 10 to 13 inches (250 to 330 mm) tall at the withers and weighs from 30 to 38 pounds (14 to 17 kg) for the male and 25 to 34 pounds (11 to 15 kg) for the female.[13]
+        
+        Life expectancy is 12–16 years.[14] Litter size can vary; usually four to six puppies.
+        
+        Health
+        The most common causes of death for the breed were cancer (28.3%), old age (24.6%), and neurological disorders (15.2%).[15][16]
+        
+        Canine Intervertebral Disc Disease (IVDD) is known to occur in the Cardigan Welsh Corgi. This is likely due to the Cardigan being a dwarf (chondrodysplastic) breed, and these breeds frequently suffer from Type I disk disease. This disease is commonly found in the Dachshund breed.[17]
+        
+        Temperament
+        Cardigan Welsh Corgis compete in dog agility trials, obedience, showmanship, flyball and tracking events. Herding instincts and trainability can be measured at noncompetitive herding tests. Corgis exhibiting basic herding instincts can be trained to compete in herding trials.[18]
+        
+        Cardigans are highly intelligent, active, athletic dogs.
+        Housepet: They have proven themselves as excellent companion animals, Cardigans are affectionate and devoted.
+        Competitive in sheepdog trials, dog agility, competitive obedience and rally obedience.
+        Guard Dogs: Capable as guard dogs in spite of their small size.`
 
     },
     {
         key:2,
         id:2,
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwB1dbtll0wh_GXYILfam-1hW3vw-Hva_G1w&usqp=CAU",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwB1dbtll0wh_GXYILfam-1hW3vw-Hva_G1w&usqp=CAU.jpg",
         petbreed:"Sample3",
         price:900,
-        decription:""
+        description:`The English Shepherd is a medium-sized breed of dog, it weighs between 40 and 60 pounds (18 and 27 kg) and stands between 18 and 23 inches (46 and 58 cm), dogs are typically larger than bitches.The breed has a long, straight or wavy double coat with feathering on the legs and tail, traditionally it has four different color combinations, black and white, black and tan, sable and white or tricolor (black, white and tan), although shades of fawn and red tan to white are also seen in the breed.According to the breed standard, the coat may be straight, wavy or curly, except on the face, skull and front of legs where it is short and smooth.
+
+        History
+        The progenitors of the English Shepherd were collies of various types brought to the New World by early settlers from Britain and Ireland, as such it shares ancestry with the modern Border Collie, Scotch Collie and Australian Shepherd.The ancestors of the English Shepherd were used extensively on farms throughout the eastern states, principally as a sheep and cattle herder, although it also performed the roles of watchdog, ratter and companion.
+        
+        The United Kennel Club (UKC) has been registering English Shepherds since 1927, originally the UKC called the breed the American Farm Shepherd but in 2003 renamed it the English Shepherd.[2] The breed is not recognized by the American Kennel Club.`
     },
     {
         key:3,
@@ -34,7 +68,11 @@ const pets=[
         image:"https://www.petaindia.com/wp-content/uploads/2014/03/Jack.JPG",
         petbreed:"Sample4",
         price:700,
-        decription:""
+        description:`The beagle is a breed of small scent hound, similar in appearance to the much larger foxhound. The beagle was developed primarily for hunting hare (beagling). Possessing a great sense of smell and superior tracking instincts, the beagle is the primary breed used as a detection dog for prohibited agricultural imports and foodstuffs in quarantine around the world. The beagle is intelligent. It is a popular pet due to its size, good temper, and a lack of inherited health problems.
+
+        The modern breed was developed in Great Britain around the 1830s from several breeds, including the Talbot Hound, the North Country Beagle, the Southern Hound, and possibly the Harrier.
+        
+        Beagles have been depicted in popular culture since Elizabethan times in literature and paintings, and more recently in film, television, and comic books`
 
     },
     {
@@ -43,7 +81,56 @@ const pets=[
         image:"https://upload.wikimedia.org/wikipedia/commons/9/9b/Indian_Cat.jpg",
         petbreed:"Sample5",
         price:400,
-        decription:""
+        description:`The Van cat (Turkish: Van kedisi; Western Armenian: Վանայ կատու, romanized: Vana gadu or Eastern Armenian: Վանա կատու, romanized: Vana katu; Kurdish: pisîka Wanê) is a distinctive landrace of the domestic cat found in the Lake Van region of the Armenian Highlands, now located in Turkey.
+
+        Van cats are relatively large, have a chalky white coat, sometimes with ruddy coloration on the head and hindquarters, and have blue or amber eyes or have heterochromia (having one eye of each colour).[1][2][3] The variety has been referred to as "the swimming cat", and has been observed to swim in Lake Van.[4][5]
+        
+        The naturally occurring Van cat type is popularly believed to be the basis of the Turkish Van breed,[1] as standardised and recognised by many cat fancier organizations; it has been internationally selectively bred to consistently produce the ruddy head-and-tail colouring pattern on the white coat. However, one of the breed founders' own writings indicate that the four original cats used to found the formal breed came from other parts of Turkey than the Lake Van area.[6][4] The capitalised and run-together term "Turkish Vankedisi" is confusingly used by some organisations as a name for all-white specimens of the formal Turkish Van breed.
+        Armenian
+        Armenians often consider the breed to be historically Armenian,[8] as the Lake Van area was inhabited by Armenians since antiquity until their local extermination during the genocide of 1915.Some authors associate the cat with the Armenian people, a population of whom have historically lived in the Lake Van area,who have been said to have "revered" the cat.Prior to 1915, the area had a large Armenian population, and the Armenian homeland is centred on Lake Van,[13] which was important even in ancient Armenian culture.[14] The Armenian inhabitants of Van have been said to have "loved" Van cats.
+        
+        Among them was post-impressionist and surrealist artist Arshile Gorky, later an immigrant to the United States, who sculpted Van cats in the early 1910s.Armenian writer Vrtanes Papazian wrote a short novel in which the cat has been used as a symbol of the Armenian liberation movement.Armenian authors Raffi,Axel Bakunts,and Paruyr Sevak have featured Van cats in their works.
+        
+        Kurdish
+        The Van region has a large Kurdish population, and Van cats have been referred to as "Kurdish cats". Some media sources reported that Turkish soldiers poisoned about 200 Van cats. These claims ultimately seem to have come from an animal rescue group called SOS Van Cats Rescue Action, a spokesperson for which stated: "The cats are Kurdish, and the Turkish authorities are unable to digest this."Van University's Van cat breeding project responded: "That the Turkish army would be able to find 200 Van cats, let alone poison them, is utter nonsense".[20]
+        
+        History
+        
+        A Van kitten from the village of Agarti (formerly Ayanis), near the city of Van, 2005
+        Van cats have been reported living in the vicinity of the city of Van and the general Lake Van area for centuries;[4] how long is uncertain. Genetic research has shown that the domestic cat's ancestor, the African wild cat (Felis lybica lybica), was domesticated, for rodent control, about 9,000 years ago in the Near East[22][23] when tribes transitioned from hunter-gathering to crop farming and settled life.In addition, the white-spotting in domestic cats appeared at the earliest stage of cat domestication, and is one of the points of evidence of early artificial selection.[26] However, this does not necessarily mean that white cats have been in the Van area the entire time.
+        
+        The landrace is named Van kedisi (plural kedileri) in Turkish, Vana gadu or Vana katu (Վանա կատու) in Western and Eastern Armenian, respectively, [27][28] and pisîka Wanê in Kurdish.
+        
+        Characteristics
+        
+        An odd-eyed Van kitten
+        Van cats are all-white,or sometimes mostly white with amber markings around the tail and ears. Locals to the Van area identify only the all-white type as Van cats, according to a 1991 BBC documentary, Cats, written and presented by Roger Tabor.[5]
+        
+        Their most notable genetic characteristic is their almond-shaped eyes that often are mismatched colours.[1] The most valued and valuable members of the type generally have one amber-green eye and one blue eye.
+        
+        Behaviour
+        Van cats are known for swimming in Lake Van.[5] This may be the source of the popular, but possibly false or exaggerated,[29] belief that the formal Turkish Van breed is innately more fond of water than the average cat. Lushington wrote: "Apart from their great capacity for affection and alert intelligence, their outstanding characteristic is their liking for water, not normally regarded as a feline attribute. They not only dabble in water and play with it, but have been known to enter ponds and even horse-troughs for a swim – they soon became famous as the 'swimming cats.'"[4] It is unclear if Lushington means the cats of the Lake Van area, or her own Turkish Van standardised breed. Tabor's BBC documentary states: "The reason for [its] fame is that the Van cat is known as 'the swimming cat'.... [H]ere at Lake Van ... these cats do enter it, and swim."[5]
+        
+        Current status
+        
+        An adult female Van cat photographed at the Van Cat Research Centre, Yüzüncü Yıl University, 2006
+        Van cats form a landrace (naturally occurring, free-breeding variety, often feral), not a standardized breed of cat.[30] They can still be found in east Turkey, near Lake Van, although their numbers have diminished (a 1992 survey found only 92 pure Van cats in their native area).[31]There is a breeding programme for the all-white variety,[32] operated by the Van Cat Research Centre (a.k.a. the Van Cat House), established in 1993 at the campus of Yüzüncü Yıl University.[33] As of 2018, the center housed about 350 young adults and kittens, and is open to the public for a nominal entrance fee.[34]
+        
+        In regional culture
+        Van cats are claimed as a cultural icon by Armenians, Kurds, and Turks, who have inhabited the region at different periods in history.[35]
+        
+        Ottoman and modern Turkish
+        Cat breeder Laura Lushington (co-founder of the "Turkish Van" formal breed, from cats procured from various parts of Turkey), wrote of the local Van cats, "they are much loved and prized by the Turks for their exceptional character and unique colouring."[4]
+        
+        Turkish folklore has it that a Van cat was aboard Noah's Ark, and that as the flood waters receded, Allah (God) blessed the cat with a ruddy patch of fur on its head when it left the ark, after which it made its way to the city of Van via Mount Ararat.[1] Many Van cats are all-white, however.[2]
+        
+        At the end of the 19th century, Sultan Abdul Hamid II is said to have owned a Van cat, and having one is still seen as a status symbol: a Prime Minister of Turkey received one as a gift, and an ambassador from Greece put himself on a waiting list to get one. Kittens from the breeding programme could be purchased for the equivalent of US$282 in 2011,[36][full citation needed] and currently the Turkish Ministry of Food, Agriculture and Livestock does not regulate the export of these or any other cats from Turkey.[37] The 1991 BBC documentary indicated that the all-white cats are locally prized in Van, and even coveted by visitors. While most Van cats are feral street cats, tourists have been known to steal pet Van cats from residents, including a shopkeeper interviewed for the show.[5]
+        
+        During the late 1990s, Van cats emerged as an informal municipal symbol of the city of Van – an enormous statue of a Van cat and her kitten now stands at the entrance to the city. The cat appears in a locally published comic strip, and in the logos of bus companies, shopping centres, and various other Van businesses.[citation needed]
+        
+        The mascot of the 2010 FIBA World Championship of basketball, hosted by Turkey, was an anthropomorphised Van cat named Bascat. He had a white coat and odd eyes, one blue and one green, and his head was shaped with design cues from the crescent moon on the Turkish flag.[38]
+        
+        `
     },
    
     {   
@@ -52,7 +139,33 @@ const pets=[
         image:"https://www.oneindia.com/img/1200x80/2020/09/310621c30yhdmg-1599120415.jpg",
         petbreed:"Sample6",
         price:1000,
-        decription:""
+        description:`The Kanni (கன்னி), meaning pure, (also known as the Maiden's Beastmaster) is a rare indigenous South Indian sighthound breed of dog found in the state of Tamil Nadu.[2] Their native breeding tract is located around villages in the Tirunelveli and Thoothukudi districts. The breed is used mainly for coursing. "Kanni" refers to the black and tan and black and sable varieties, while the variety which is one colour is known as the Chippiparai. However some experts claim both Kanni and Chippiparai dogs to be distinct dogs.
+        History
+        In the beginning Kanni dogs existed only in several pockets of Southern Tamilnadu especially in Southern districts like Northern most part of Tirunelveli, Tuticorin, Southern part of Virudhunagar.
+        
+        Name
+        The name Kanni means "pure" in Tamil and was given to this breed for its loyalty – purity of heart. The Kanni is also known as the Maiden's Beastmaster for its voracity with which it defends its territory against other wild animals and because they are often gifted to newlywed brides as their guardians.[4] Although Kannis come in different colours, generally only the ones that were black and tan were given as marriage gifts. This led to the wrong naming of the black and tan coloured breeds as Kanni in dogs as well as goats in future.[5] Even though the native people call this breed as Kanni, Kennel Club of India (KCI) registered this with two names. It registered the black and tan coloured dogs of this breed as Kanni and the rest as Chippiparai respectively.
+        
+        Characteristics
+        Kannis are medium-sized dogs, with straight top lines and tucked-up abdomens. Height at the withers is 64–74 centimetres (25–29 in) and weight ranges from 16–22 kilograms (35–49 lb).[6] They have straight heads, strong jaws, golden eyes and black noses. The ears are medium-sized and flat, and can be erect, dropping or semi dropping. The tail is semi curved.[7] Kannis have a lifespan of 14 to 16 years.[6]
+        
+        Colour types
+        
+        Paal Kanni
+        Kannis have short coats, which can be brown with black, fawn (Santhana Pillai), light red (Sevalai) to dark red (Karum Sevalai), cream, white (Mayila Pillai), red with white, or pale grey (Sambal).[8]
+        
+        Other colours include black with fawn markings (Paal Kanni), black with dark tan markings (Seng Kanni), black with fawn face and legs (Karung Kanni), fawn head and legs with rest of body being black (Parukki), and brown and white (Semmarai).
+        
+        Temperament
+        The Kanni is usually shy but will always defend its home or master, if the need arises. The Kanni dogs are extremely faithful and easy to train but they will always think independently when on a hunt. They are very loyal to their family.[6]
+        
+        Hunting
+        Kannis are specially bred and used for hunting Indian hares. Like other hounds, Kannis use their speed and endurance to hunt their prey. They use their excellent sense of smell and sight to track and hunt their prey. Kannis are trained to obey subtle hand signals of their masters while on the hunt. Kanni dogs have been known to hit speeds up to 60 km/h.[9] The key to the Kanni's capability as a coursing dog can be found in its light but muscular build, large heart, double suspension gallop, and extreme flexibility of its spine all like that of Cheetah.[2]
+        
+        Habitat and food habits
+        Kannis live in a temperate climate zone. They need free space and their own territory, and are not suited to city life. Kannis eat millet, corn, jowar, rice porridge with milk or curd, and non-vegetable foods.[10]
+        
+        `
 
     }
 ]
