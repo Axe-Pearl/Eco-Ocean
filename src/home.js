fabbbeee9e23c6f7c6df19a8e01fe7f4faa14d4c
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-24 22:04:32 +0530

    Home Page design update 1.2

--- src/home.js
+++ src/home.js
@@ -14,6 +14,7 @@ function Home() {
                     <p className="description">Let's help them...</p>
                     <p className="donate-line">Your Contribution matters, Express Your Soul and Donate </p>
                     <button className="donate_button" type="button" ></button>
+                    <button className="signup_button" type="button" >SIGNUP</button>
                    </div>
                    <div className="image-container1">
                        <img className="screen" src="../Images/intro-section-image01.svg"></img>
@@ -21,8 +22,22 @@ function Home() {
                    </div>
               </div>
             </div>
-            <div className="learn-more">
-                <div className="qrcode">
+            <div className="adopt">
+                <div className="image-section">
+                <img className="image" src="../Images/adopt.jpg"></img>
+                </div>
+                <div className="why-section">
+                    <h4 className="why-title">
+                        Why you should <br></br>Adopt them ?
+                    </h4>
+                    <div className="why">
+                    <p >Adopting pets are amazing !!Not only it<br></br> helps them but there are numerous profits to have a pet.<br></br>Having pets decreases stress and loneliness<br></br>at great level,there are several research that<br></br>that tells how it lowers several mental,physcial illness<br></br> and cares your heart ❤️. <br></br> <a href="https://www.humanesociety.org/resources/top-reasons-adopt-pet" className="linker"></a> </p>
+                    </div>
+                    
+                </div>
+            </div>
+            {/* <div>
+            <div className="qrcode">
                     <img  src="../Images/cellphone.jpg"></img>
                     <div className="code">
                        <QRCode
@@ -32,10 +47,9 @@ function Home() {
                         level={"H"}
                         includeMargin={true}
                         />
-
-                    </div>
+                        </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
