bd437151d9ca25a73909aa5a0a99b1e1644a20cc
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-25 02:17:05 +0530

    our mission and jab we met section added,design update 1.4

--- src/home.js
+++ src/home.js
@@ -61,7 +61,7 @@ function Home() {
                 </div>
             </div>
 
-            <div className="container">
+            <div className="containerqr">
             <div className="qrcode">
                     <img  src="../Images/cellphone.jpg"></img>
                     <div className="code">
@@ -77,7 +77,7 @@ function Home() {
             <div className="why-section2">
                     <h4 className="why-title2">
                         Have more Questions ?
-                      <br></br> Scan QRCode to Know more and download pdf!
+                      <br></br> Scan QRCode to Know more and <br></br>download pdf!
                     </h4>
                     <div className="why2">
                     <p >You can read a well defined guide here,<br></br>
@@ -88,6 +88,86 @@ function Home() {
                 </div>
             <div className="image-section2 image-container">
                 <img className="image" src="../Images/askme.jpg"></img>
+            </div>
+            </div>
+
+            <div className="mission">
+            <div>
+                    <h4 className="mission-title">
+                       OUR MISSION
+                    </h4>
+                    <div className="about-mission">
+                    <p>There are Millions of street Animals in India, many of them are relinquished by their masters because
+                        <br></br> they seems no more important to them,they are living in very bad condition,
+                        <br></br>they die or do suffer whole life,by accident,starvation or by eating polythenes or plastics
+                        <br></br>all we earn by donation or selling pets,we serves street animals,
+                        <br></br>our mission is to help each and every animal in India
+                        <br></br>We rescue street Animals and transform them to be ready as pet 🐶</p>
+                    </div>
+
+              <div className="container">
+                <div className="image-section">
+                <img className="image" src="../Images/report1.jpg"></img>
+                </div>
+                <div className="why-section">
+                    <h4 className="why-title">
+                        All we need is <br></br>to care
+                    </h4>
+                    <div className="why">
+                    <p className="why">According to one report of Times of India,
+                    <br></br> 1,000 cows die every year in Lucknow after eating polythene,
+                    <br></br> in post-mortem reports 50-55kg of polythene is found in stomach of street cows. 
+                    <br></br>They die in too much pain.This is the shameful act of humans
+                    <br></br> still there is no remorse  </p>
+                    </div>
+                    
+                </div>
+                </div>
+                </div>
+            </div>
+
+         <div className="meet">
+         <h4 className="mission-title">
+                       Jab We Met
+                    </h4>
+            <div className="container-work">
+                <div className="our-work">
+                    <div className="image-work">
+                <img className="image-works" src="../Images/rescue1.jpg"></img>
+                </div>
+                    <div className="story">
+                        <h2>'Sakshi Singh' & her dog 'Hero'</h2>
+                    <p className="words">"I never thought street dogs can be <br></br>so calm and lovely, he is very intelligent
+                    <br></br>,thanks <strong>ECO-OCEAN</strong> for meeting my dog <strong>HERO </strong>"</p>
+                    </div>
+                    
+                </div>
+
+                <div className="our-work">
+                    <div className="image-work">
+                <img className="image-works" src="../Images/customer1.jpg"></img>
+                </div>
+                    <div className="story">
+                    <h2>'Emma Malhotra' & her cat 'Teena'</h2>
+                    <p className="words">"She was shy at first but cute and lovely <br></br>from early,she 
+                    was already got injections<br></br> from <strong>ECO-OCEAN</strong>
+                    ,my life changed after having
+                    <br></br> <strong>Teena</strong> with me."</p>
+                    </div>
+                    
+                </div>
+
+                <div className="our-work">
+                    <div className="image-work">
+                <img className="image-works" src="../Images/customer3.jpg"></img>
+                </div>
+                <div className="story">
+                    <h2>'Rashmi' & her dog 'Sheroo'</h2>
+                    <p className="words">"Love that I saved his life<br></br> and he make my life better, 
+                    <br></br>thanks <strong>ECO-OCEAN</strong>,for having <strong>Sheroo</strong> with me."</p>
+                    </div>
+                    
+                </div>
             </div>
             </div>
         </div>
