07fbaae937380df3cfe6d385ad72888e3c633582
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-26 00:31:16 +0530

    stories-section modified,design update 2.1

--- src/signup.js
+++ src/signup.js
@@ -1,10 +1,59 @@
 import React from "react";
-
-
+import { Link } from "react-router-dom";
+import PetsIcon from '@material-ui/icons/Pets';
+import "./signup.css";
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 function Signup(){
     return(
        <div>
-         <h1>Hello</h1>
+        <div className="sign-up">
+                <Link to="/" className="homepage"><h1 className="webname"><PetsIcon classes={{ root: 'logo-icon'}} style={{ fontSize: 30 }}/>Eco Ocean</h1></Link> 
+                <div className="app-tag">
+                    <p className="tag quote">
+                        "VASUDEV KUTUMBKAM"
+                       
+                    </p>
+                    <p className="tag">
+                    The Whole World is our Family
+                    </p>
+                </div>
+                <div className="image-animals">
+                 <img  className="img" src="../Images/donatethem.jpg"></img>
+                </div>
+                <div className="app-tag">
+                    <p className="tag quote">
+                       Let's Come Together
+                       
+                    </p>
+                    <p className="tag">
+                    Help them to get a good life.
+                    </p>
+                </div>
+                <div className="connect-us">
+                <small>Connect with us:</small>
+                <div className="icon-container">
+                <TwitterIcon classes={{root:"icons-ani"}} />
+                <Link to="https://www.linkedin.com/in/anurag-nema-8b5569194/"><LinkedInIcon classes={{root:"icons-ani"}} /></Link>
+                <Link to="https://github.com/Axe-Pearl"><GitHubIcon classes={{root:"icons-ani"}} /></Link>
+                </div>
+            </div>
+       </div>
+
+       <div className="signup-container">
+           <h1>Sign In</h1>
+           <div className="signup-form">
+               <label className="labels" for="email">Email</label>
+               <input className="input-fields" type="text" placeholder="ram@example.com"></input>
+
+               <label className="labels" for="email">Password</label>
+               <input className="input-fields" type="password" placeholder="password"></input>
+
+               <button className="btn" type="submit">SIGN IN</button>
+               <p className="signin-line">Don't have an account ?&ensp; <a className="linkit" href="#">Sign In</a></p>
+           </div>
+       </div>
        </div>
     );
 }
--- src/stories.js
+++ src/stories.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import ReactPlayer from "react-player";
 import videos from "./storycontent";
+import Footer from "./footer";
 import "./stories.css";
 function Stories() {
     return (
         <div>
+            <div className="whythis">
+                <div className="image-container saveus">
+                    <img src="../Images/animaldream.jpg"></img>
+                </div>
+                <div className="story-why">
+                     <h3 className="why-title">🚫 Say No! No to Animal Cruelty</h3>
+                     <div className="why">
+                         <p>Animal Cruelty is a inhumanity,sin,illegal,<br></br>
+                         whatever we say,it's wrong.Let's prove we are Humans<br></br>
+                         Report any abuse around you,whether it's any occassion/festival<br></br>
+                         Just say no to Crackers</p>
+                     </div>
+                </div>
+            </div>
+            <div className="motto">
+                <h2 className="slogan">'VASUDEV KUTUMBKAM'</h2>
+                <p className="slogan-mean">-- The Whole World is our Family --</p>
+                <p className="motive">The whole motivation of our Mission is this belief, that we are family
+                <br></br>,a ecological family that shares mother Earth.
+                <br></br>Let's Come together and help animals,
+                <br></br>Let's Fight together against Cruelty and help them to overcome bad.</p>
+            </div>
             <div className="vedio-container">
+                <div className="title-stories">
+                    <h1>Let's get Aware - Stories/Documenteries</h1>
+                </div>
             {videos.map((links)=>{
                 return(
                     <div className="video_container">
@@ -24,7 +50,9 @@ function Stories() {
 
             })
             }
-           
+           <div className="footer">
+                <Footer />
+            </div>
             </div>
         </div>
     )
