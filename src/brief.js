75fe1032033ddbea78becff81788f26b09d67cdc
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-06-26 23:58:22 +0530

    read-more page modified,design update 6.1

--- src/brief.js
+++ src/brief.js
@@ -9,11 +9,10 @@ function Brief() {
     return (
         <div className ="brief-container">
             <div className="pet_img">
+                <h1>Everything About the Breed </h1>
                 <img className="image" src={reader[0].image}></img>
-                <h1 className="breed">{reader[0].petbreed}</h1>
+                <h1 className="breedo">{reader[0].petbreed}</h1>
                 <h5 className="tagline">Hey Humans! Before you adopt me,let's learn about me </h5>
-            </div>
-            <div className="description-box">
                 <h3 className="introduction">Who I'm Exactly ?</h3>
                 <p className="about_pet">{reader[0].description}</p>
             </div>
